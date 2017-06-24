1151ecbc45ded69f4a39a8c2f38828c15aa52bac
David Indra <mail@davidindra.cz>
2017-06-24 13:05:08 +0200

    Binaries

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,11 +29,241 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBoxAddress = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.updaterTimer = new System.Windows.Forms.Timer(this.components);
+            this.scheme = new System.Windows.Forms.PictureBox();
+            this.labelX = new System.Windows.Forms.Label();
+            this.labelY = new System.Windows.Forms.Label();
+            this.labelVX = new System.Windows.Forms.Label();
+            this.labelVY = new System.Windows.Forms.Label();
+            this.labelFuel = new System.Windows.Forms.Label();
+            this.labelLandingL = new System.Windows.Forms.Label();
+            this.labelLandingR = new System.Windows.Forms.Label();
+            this.labelID = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonStartAuto = new System.Windows.Forms.Button();
+            this.labelState = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.scheme)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxAddress
+            // 
+            this.textBoxAddress.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxAddress.Location = new System.Drawing.Point(99, 7);
+            this.textBoxAddress.Name = "textBoxAddress";
+            this.textBoxAddress.Size = new System.Drawing.Size(96, 20);
+            this.textBoxAddress.TabIndex = 0;
+            this.textBoxAddress.Text = "127.0.0.1:8000";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(201, 7);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(133, 20);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "Manuální přistání";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Adresa serveru:";
+            // 
+            // updaterTimer
+            // 
+            this.updaterTimer.Enabled = true;
+            this.updaterTimer.Interval = 50;
+            this.updaterTimer.Tick += new System.EventHandler(this.updaterTimer_Tick);
+            // 
+            // scheme
+            // 
+            this.scheme.BackColor = System.Drawing.Color.White;
+            this.scheme.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.scheme.Location = new System.Drawing.Point(15, 33);
+            this.scheme.Name = "scheme";
+            this.scheme.Size = new System.Drawing.Size(450, 450);
+            this.scheme.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.scheme.TabIndex = 3;
+            this.scheme.TabStop = false;
+            // 
+            // labelX
+            // 
+            this.labelX.AutoSize = true;
+            this.labelX.Location = new System.Drawing.Point(79, 490);
+            this.labelX.Name = "labelX";
+            this.labelX.Size = new System.Drawing.Size(14, 13);
+            this.labelX.TabIndex = 4;
+            this.labelX.Text = "X";
+            // 
+            // labelY
+            // 
+            this.labelY.AutoSize = true;
+            this.labelY.Location = new System.Drawing.Point(128, 490);
+            this.labelY.Name = "labelY";
+            this.labelY.Size = new System.Drawing.Size(14, 13);
+            this.labelY.TabIndex = 5;
+            this.labelY.Text = "Y";
+            // 
+            // labelVX
+            // 
+            this.labelVX.AutoSize = true;
+            this.labelVX.Location = new System.Drawing.Point(193, 490);
+            this.labelVX.Name = "labelVX";
+            this.labelVX.Size = new System.Drawing.Size(20, 13);
+            this.labelVX.TabIndex = 6;
+            this.labelVX.Text = "vX";
+            // 
+            // labelVY
+            // 
+            this.labelVY.AutoSize = true;
+            this.labelVY.Location = new System.Drawing.Point(242, 490);
+            this.labelVY.Name = "labelVY";
+            this.labelVY.Size = new System.Drawing.Size(20, 13);
+            this.labelVY.TabIndex = 7;
+            this.labelVY.Text = "vY";
+            // 
+            // labelFuel
+            // 
+            this.labelFuel.AutoSize = true;
+            this.labelFuel.Location = new System.Drawing.Point(320, 490);
+            this.labelFuel.Name = "labelFuel";
+            this.labelFuel.Size = new System.Drawing.Size(14, 13);
+            this.labelFuel.TabIndex = 8;
+            this.labelFuel.Text = "P";
+            // 
+            // labelLandingL
+            // 
+            this.labelLandingL.AutoSize = true;
+            this.labelLandingL.Location = new System.Drawing.Point(383, 490);
+            this.labelLandingL.Name = "labelLandingL";
+            this.labelLandingL.Size = new System.Drawing.Size(13, 13);
+            this.labelLandingL.TabIndex = 9;
+            this.labelLandingL.Text = "L";
+            // 
+            // labelLandingR
+            // 
+            this.labelLandingR.AutoSize = true;
+            this.labelLandingR.Location = new System.Drawing.Point(420, 490);
+            this.labelLandingR.Name = "labelLandingR";
+            this.labelLandingR.Size = new System.Drawing.Size(15, 13);
+            this.labelLandingR.TabIndex = 10;
+            this.labelLandingR.Text = "R";
+            // 
+            // labelID
+            // 
+            this.labelID.AutoSize = true;
+            this.labelID.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelID.Location = new System.Drawing.Point(20, 490);
+            this.labelID.Name = "labelID";
+            this.labelID.Size = new System.Drawing.Size(20, 13);
+            this.labelID.TabIndex = 11;
+            this.labelID.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(12, 522);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(438, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Modul můžete ovládat při manuálním přistání klávesnicí šipkami nahoru, doleva a d" +
+    "oprava.";
+            // 
+            // buttonStartAuto
+            // 
+            this.buttonStartAuto.Location = new System.Drawing.Point(341, 7);
+            this.buttonStartAuto.Name = "buttonStartAuto";
+            this.buttonStartAuto.Size = new System.Drawing.Size(124, 20);
+            this.buttonStartAuto.TabIndex = 13;
+            this.buttonStartAuto.Text = "Automatické přistání";
+            this.buttonStartAuto.UseVisualStyleBackColor = true;
+            this.buttonStartAuto.Click += new System.EventHandler(this.buttonStartAuto_Click);
+            // 
+            // labelState
+            // 
+            this.labelState.AutoSize = true;
+            this.labelState.BackColor = System.Drawing.Color.White;
+            this.labelState.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelState.Location = new System.Drawing.Point(23, 42);
+            this.labelState.Name = "labelState";
+            this.labelState.Size = new System.Drawing.Size(152, 13);
+            this.labelState.TabIndex = 14;
+            this.labelState.Text = "Přistání nebylo zahájeno.";
+            this.labelState.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 541);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(245, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Přerušit probíhající přistání můžete klávesou Esc.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(477, 568);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.labelState);
+            this.Controls.Add(this.buttonStartAuto);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelID);
+            this.Controls.Add(this.labelLandingR);
+            this.Controls.Add(this.labelLandingL);
+            this.Controls.Add(this.labelFuel);
+            this.Controls.Add(this.labelVY);
+            this.Controls.Add(this.labelVX);
+            this.Controls.Add(this.labelY);
+            this.Controls.Add(this.labelX);
+            this.Controls.Add(this.scheme);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.textBoxAddress);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Přistání na Měsíci";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.scheme)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxAddress;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer updaterTimer;
+        private System.Windows.Forms.PictureBox scheme;
+        private System.Windows.Forms.Label labelX;
+        private System.Windows.Forms.Label labelY;
+        private System.Windows.Forms.Label labelVX;
+        private System.Windows.Forms.Label labelVY;
+        private System.Windows.Forms.Label labelFuel;
+        private System.Windows.Forms.Label labelLandingL;
+        private System.Windows.Forms.Label labelLandingR;
+        private System.Windows.Forms.Label labelID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonStartAuto;
+        private System.Windows.Forms.Label labelState;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,344 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Globalization;
+using mesic.Properties;
 
 namespace mesic
 {
     public partial class Form1 : Form
     {
+        string serverAddress;
+        int landingID;
+        LandingState lastState;
+
+        bool autoland;
+        int maxSpeed;
+
         public Form1()
         {
             InitializeComponent();
+
+            landingID = -1; // landing is not running right now
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // start landing automatically if called from command line
+            string[] args = Environment.GetCommandLineArgs();
+            if (args.Length == 3)
+            {
+                autoland = true;
+                maxSpeed = int.Parse(args[2]);
+
+                textBoxAddress.Text = args[1];
+                serverAddress = args[1];
+                lastState = makeRequest(serverAddress + "/start");
+                landingID = lastState.id;
+
+                disableControls();
+            }
+        }
+
+        // handles making requests to the server w/ parsing response into LandingState struct defined at the bottom
+        private LandingState makeRequest(string address)
+        {
+            string text = string.Empty;
+
+            // do HTTP request
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://" + address);
+            request.AutomaticDecompression = DecompressionMethods.GZip; // to be sure
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                text = reader.ReadToEnd(); // get the response
+            }
+
+            LandingState result = new LandingState();
+
+            string[] linesArray = text.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);
+
+            if(linesArray[0] == "Zdar!")
+            {
+                result.state = LandingState.SUCCESS;
+                return result;
+            }
+
+            if (linesArray[0] == "Krach!")
+            {
+                result.state = LandingState.FAILURE;
+                return result;
+            }
+
+            // parsing response into struct
+            result.state = LandingState.LANDING;
+            result.id = int.Parse(linesArray[0]);
+
+            result.moduleX = float.Parse(linesArray[1].Split(new char[] { ' ' })[0], CultureInfo.InvariantCulture);
+            result.moduleY = float.Parse(linesArray[1].Split(new char[] { ' ' })[1], CultureInfo.InvariantCulture);
+
+            result.velocityX = float.Parse(linesArray[2].Split(new char[] { ' ' })[0], CultureInfo.InvariantCulture);
+            result.velocityY = float.Parse(linesArray[2].Split(new char[] { ' ' })[1], CultureInfo.InvariantCulture);
+
+            result.fuel = float.Parse(linesArray[3], CultureInfo.InvariantCulture);
+
+            result.landingL = float.Parse(linesArray[4].Split(new char[] { ' ' })[0], CultureInfo.InvariantCulture);
+            result.landingR = float.Parse(linesArray[4].Split(new char[] { ' ' })[1], CultureInfo.InvariantCulture);
+
+            return result;
+        }
+
+        // occurs when manual landing button is clicked
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            autoland = false;
+
+            serverAddress = textBoxAddress.Text;
+            lastState = makeRequest(serverAddress + "/start");
+            landingID = lastState.id;
+
+            disableControls();
+        }
+
+        // occurs when autolanding button is clicked
+        private void buttonStartAuto_Click(object sender, EventArgs e)
+        {
+            autoland = true;
+
+            serverAddress = textBoxAddress.Text;
+            lastState = makeRequest(serverAddress + "/start");
+            landingID = lastState.id;
+
+            disableControls();
+        }
+
+        // calculates autolanding moves and sends the requests
+        private void calculateAutoLand(LandingState state)
+        {
+            bool thrustUp = false;
+            bool thrustLeft = false;
+            bool thrustRight = false;
+
+            // vertical
+            if ((state.velocityY < -4 && state.moduleY < 40) ||
+                (state.velocityY < -8 && state.moduleY < 100) ||
+                (state.velocityY < -12 && state.moduleY < 150) ||
+                (state.velocityY < -15 && state.moduleY < 160))
+            {
+                thrustUp = true;
+            }
+
+            // to fit the board
+            if((state.moduleX - 20 < state.landingL))
+            {
+                thrustRight = true;
+            }
+
+            if((state.moduleX + 20 > state.landingR))
+            {
+                thrustLeft = true;
+            }
+
+            // horizontal overspeed
+            if((state.velocityX > 5))
+            {
+                thrustRight = false;
+                thrustLeft = true;
+            }
+
+            if((state.velocityX < -5))
+            {
+                thrustRight = true;
+                thrustLeft = false;
+            }
+
+            // create and send the request
+            string url = serverAddress + "/stav?id=" + landingID;
+            if (thrustUp) url += "&nahoru=1"; else url += "&nahoru=0";
+            if (thrustLeft) url += "&doleva=1"; else url += "&doleva=0";
+            if (thrustRight) url += "&doprava=1"; else url += "&doprava=0";
+            makeRequest(url);
+        }
+
+        // periodic call, fetches actual data and triggers autolanding function
+        private void updaterTimer_Tick(object sender, EventArgs e)
+        {
+            if(landingID != -1)
+            {
+                LandingState state = makeRequest(serverAddress + "/stav?id=" + landingID);
+                if (autoland) calculateAutoLand(state); // autoland calculations
+
+                showState(state);
+
+                switch (state.state)
+                {
+                    case LandingState.SUCCESS:
+                        landingID = -1;
+                        //MessageBox.Show("Přistání bylo úspěšné!", "Stav přistání", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        enableControls();
+                        break;
+
+                    case LandingState.FAILURE:
+                        landingID = -1;
+                        //MessageBox.Show("Přistání se nezdařilo.", "Stav přistání", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        enableControls();
+                        break;
+
+                    case LandingState.LANDING:
+                        showData(state);
+                        drawScheme(state);
+
+                        lastState = state;
+                        break;
+                }
+            }
+        }
+
+        // output the data into information labels
+        private void showData(LandingState state)
+        {
+            labelID.Text = "ID: " + state.id.ToString();
+
+            labelX.Text = "X: " + Math.Round(state.moduleX, 1).ToString();
+            labelY.Text = "Y: " + Math.Round(state.moduleY, 1).ToString();
+
+            if (state.velocityX > -5 && state.velocityX < 5) labelVX.ForeColor = Color.Green;
+            if (state.velocityX < -5 || state.velocityX > 5) labelVX.ForeColor = Color.Red;
+            labelVX.Text = "vX: " + Math.Round(state.velocityX, 1).ToString();
+
+            if (state.velocityY > -5) labelVY.ForeColor = Color.Green;
+            if (state.velocityY < -5) labelVY.ForeColor = Color.Red;
+            labelVY.Text = "vY: " + Math.Round(state.velocityY, 1).ToString();
+
+            if (state.fuel < 5) labelFuel.ForeColor = Color.Red;
+            if (state.fuel > 5 && state.fuel < 10) labelFuel.ForeColor = Color.Orange;
+            if (state.fuel > 10) labelFuel.ForeColor = Color.Green;
+            labelFuel.Text = "P: " + Math.Round(state.fuel, 1).ToString();
+
+            labelLandingL.Text = "L: " + Math.Round(state.landingL, 1).ToString();
+            labelLandingR.Text = "R: " + Math.Round(state.landingR, 1).ToString();
+        }
+
+        // show colored state info
+        private void showState(LandingState state)
+        {
+            switch (state.state)
+            {
+                case LandingState.LANDING:
+                    labelState.Text = "Přistání probíhá...";
+                    labelState.ForeColor = Color.Orange;
+                    break;
+                case LandingState.SUCCESS:
+                    labelState.Text = "Přistání bylo úspěšné!";
+                    labelState.ForeColor = Color.Green;
+                    break;
+                case LandingState.FAILURE:
+                    labelState.Text = "Přistání se nezdařilo.";
+                    labelState.ForeColor = Color.Red;
+                    break;
+            }
+            labelState.Visible = true;
+        }
+
+        // draw the picture of the situation
+        private void drawScheme(LandingState state)
+        {
+            Bitmap bmp = new Bitmap(2000, 2000);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.Clear(Color.Transparent);
+            }
+            scheme.Image = bmp;
+
+            using (Graphics g = Graphics.FromImage(scheme.Image))
+            {
+                g.DrawLine(new Pen(Color.Black, 20), state.landingL * 10, 2000, state.landingR * 10, 2000);
+
+                object o = Resources.ResourceManager.GetObject("lunarModule");
+                Image lunarModule = (Image)o;
+                g.DrawImage(lunarModule, 10 * state.moduleX, 2000 - 10 * state.moduleY - 70);
+            }
+        }
+
+        // disabling/enabling buttons
+        private void disableControls()
+        {
+            buttonStart.Enabled = false;
+            buttonStartAuto.Enabled = false;
+            textBoxAddress.Enabled = false;
+        }
+
+        private void enableControls()
+        {
+            buttonStart.Enabled = true;
+            buttonStartAuto.Enabled = true;
+            textBoxAddress.Enabled = true;
+            textBoxAddress.Focus();
+        }
+
+        // handling keyboard manual movement
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Escape)
+            {
+                landingID = -1;
+                enableControls();
+            }
+
+            if (landingID == -1 || autoland) return;
+            switch (e.KeyCode) // sending requests by the detected keys
+            {
+                case Keys.Up:
+                    makeRequest(serverAddress + "/stav?id=" + landingID + "&nahoru=1");
+                    break;
+                case Keys.Left:
+                    makeRequest(serverAddress + "/stav?id=" + landingID + "&doleva=1");
+                    break;
+                case Keys.Right:
+                    makeRequest(serverAddress + "/stav?id=" + landingID + "&doprava=1");
+                    break;
+            }
+        }
+
+        // DTTO like higher
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (landingID == -1 || autoland) return;
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    makeRequest(serverAddress + "/stav?id=" + landingID + "&nahoru=0");
+                    break;
+                case Keys.Left:
+                    makeRequest(serverAddress + "/stav?id=" + landingID + "&doleva=0");
+                    break;
+                case Keys.Right:
+                    makeRequest(serverAddress + "/stav?id=" + landingID + "&doprava=0");
+                    break;
+            }
+        }
+    }
+
+    // server data struct
+    struct LandingState
+    {
+        public const int LANDING = 1;
+        public const int SUCCESS = 2;
+        public const int FAILURE = 3;
+
+        public int state;
+
+        public int id;
+
+        public float moduleX, moduleY, velocityX, velocityY, fuel, landingL, landingR;
+
+        public override string ToString() // debugging purposes
+        {
+            return "ID " + id + " (state " + state + ")\nX: " + moduleX + ", Y: " + moduleY + "\nvX: " + velocityX + ", vY: " + velocityY + "\nfuel: " + fuel + "\nlandingL: " + landingL + ", landingR: " + landingR;
         }
     }
 }
